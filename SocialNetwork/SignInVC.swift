6d60fdca150070455e30dffeff15f2f2fbc5fc03
COMPUTER <computer@MINI12E.local>
2017-07-24 17:01:03 -0600

    Attempted to fix vc not named properly

---
+++ SocialNetwork/SignInVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by COMPUTER on 7/24/17.
+//  Copyright © 2017 COMPUTER. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+  
+
+}
+
