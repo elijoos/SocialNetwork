5dab0984c7639b5a894e33f12f5460794a8722c8
COMPUTER <computer@MBA11.local>
2017-08-20 13:37:20 -0600

    Completed creating Firebase DB users

---
+++ SocialNetwork/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  SocialNetwork
+//
+//  Created by COMPUTER on 8/15/17.
+//  Copyright © 2017 COMPUTER. All rights reserved.
+//
+
+import Foundation
---
+++ SocialNetwork/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  SocialNetwork
+//
+//  Created by COMPUTER on 8/7/17.
+//  Copyright © 2017 COMPUTER. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
